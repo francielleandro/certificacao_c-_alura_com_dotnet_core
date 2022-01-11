0cfa40ea4effe1e57dffab70a42178dcbc003d83
Franciel Leandro <francielleandro@gmail.com>
2022-01-11 12:06:23 -0300

    refatorando

--- 7 - Enum/Enumeracoes.cs
+++ 7 - Enum/Enumeracoes.cs
@@ -6,15 +6,22 @@ using System.Threading.Tasks;
 
 namespace certificacao_csharp_roteiro
 {
-    class Enumeracoes : IAulaItem
-    {
+    class Enumeracoes : IAulaItem{
+        //definindo enumeradores
+        enum DiasDaSemana : long { Seg = 3, Ter = 10, Qua = 15, Qui, Sex, Sab, Dom }
+
+        [Flags]
+        enum DiasDeTrabalho { Seg = 0, Ter = 1, Qua = 2, Qui = 4, Sex = 8, Sab = 16, Dom = 32 }
         public void Executar()
         {
+            /* Modo de se tratar sem enumeradores
             const int Seg = 0;
             const int Ter = 1;
             const int Qua = 2;
 
             DiasDaSemana primeiroDia = DiasDaSemana.Seg;
+            Console.WriteLine(primeiroDia);
+            */
 
             DiasDeTrabalho diasDeTrabalho
                 = DiasDeTrabalho.Ter | DiasDeTrabalho.Qui | DiasDeTrabalho.Sex;
@@ -24,9 +31,5 @@ namespace certificacao_csharp_roteiro
         }
     }
 
-    enum DiasDaSemana : long { Seg = 3, Ter = 10, Qua = 15, Qui, Sex, Sab, Dom }
-
-    [Flags]
-    enum DiasDeTrabalho { Seg = 0, Ter = 1, Qua = 2, Qui = 4, Sex = 8, Sab = 16, Dom = 32 }
 
 }
